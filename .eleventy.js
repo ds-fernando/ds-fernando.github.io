0917cf6535608168f20e36310114a9ede91acb5c
Fernando Leon <fernandoleonsierra@gmail.com>
2024-12-24 23:00:57 -0500

    configuracion inicial y creacion de base layout

---
+++ .eleventy.js
@@ -0,0 +1,16 @@
+module.exports = function (eleventyConfig) {
+    eleventyConfig.addPassthroughCopy("src/css");
+    eleventyConfig.addPassthroughCopy("src/js");
+  
+
+    return{
+        dir:{
+            input: "src",
+            output: "docs"
+        },
+        templateFormats: ['md', 'njk', 'html'],
+        markdownTemplateEngine: 'njk',
+        htmlTemplateEngine: 'njk',
+        dataTemplateEngine: 'njk', };
+
+};
