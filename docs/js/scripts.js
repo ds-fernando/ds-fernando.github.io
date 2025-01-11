a856ec11913dc6d5a7e138ea89972ea5b91b4e40
Fernando Leon <fernandoleonsierra@gmail.com>
2025-01-11 15:56:07 -0500

    nav movil

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -1,8 +1,13 @@
-// Seleccionamos el botón de hamburguesa y el menú
-const menuToggle = document.querySelector('.menu-toggle');
-const menuList = document.querySelector('.menu-list');
+document.addEventListener("DOMContentLoaded", function() {
+  // Seleccionamos el botón de hamburguesa y el menú
+  const menuToggle = document.querySelector('.menu-toggle');
+  const menuList = document.querySelector('.menu-list');
 
-// Añadimos un evento al botón para alternar la clase 'show' en el menú
-menuToggle.addEventListener('click', () => {
-  menuList.classList.toggle('show');
+  // Aseguramos que los elementos existen antes de agregar el evento
+  if (menuToggle && menuList) {
+    // Añadimos un evento al botón para alternar la clase 'show' en el menú
+    menuToggle.addEventListener('click', () => {
+      menuList.classList.toggle('show');
+    });
+  }
 });
