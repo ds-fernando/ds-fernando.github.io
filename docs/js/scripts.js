56e9817129101518ac4db61ee8e8991eb46e3a59
Fernando Leon <fernandoleonsierra@gmail.com>
2025-01-11 18:49:53 -0500

    n

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -5,10 +5,15 @@ const closeBtn = document.querySelector('.close-btn');
 
 // Añadimos un evento al botón para alternar la clase 'show' en el menú
 menuToggle.addEventListener('click', () => {
-  menuList.classList.add('show');
+  menuList.classList.toggle('show');
 });
 
 // Añadimos un evento al botón de cierre para ocultar el menú
 closeBtn.addEventListener('click', () => {
   menuList.classList.remove('show');
+});
+
+menuToggle.addEventListener('click', () => {
+  menuList.classList.toggle('show');
+  menuToggle.classList.toggle('open'); // Alternar la clase 'open' en el botón
 });
