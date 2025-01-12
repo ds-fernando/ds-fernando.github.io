d286f92314ee26f461425d0143a69fccb8722222
Fernando Leon <fernandoleonsierra@gmail.com>
2025-01-11 19:09:13 -0500

    transicion hamburgesa a x

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -6,14 +6,11 @@ const closeBtn = document.querySelector('.close-btn');
 // Añadimos un evento al botón para alternar la clase 'show' en el menú
 menuToggle.addEventListener('click', () => {
   menuList.classList.toggle('show');
+  menuToggle.classList.toggle('open'); // Alternar la clase 'open' en el botón de hamburguesa
 });
 
 // Añadimos un evento al botón de cierre para ocultar el menú
 closeBtn.addEventListener('click', () => {
   menuList.classList.remove('show');
-});
-
-menuToggle.addEventListener('click', () => {
-  menuList.classList.toggle('show');
-  menuToggle.classList.toggle('open'); // Alternar la clase 'open' en el botón
+  menuToggle.classList.remove('open'); // Aseguramos que la clase 'open' se remueva al cerrar
 });
